3af9851b836fbf5e1402644d82d8777c3ca12ee4
Hi-Im-Simon <75808585+Hi-Im-Simon@users.noreply.github.com>
2022-11-04 19:53:46 +0100

    Co-authored-by: Mikołaj Bartkowiak <Angelfrost@users.noreply.github.com> Co-authored-by: dd-flex <dd-flex@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,28 @@
+// class = r-kzbkwu
+// Sejm API - IX kadencja (lista posłów) = https://api.sejm.gov.pl/sejm/term9/MP
+document.body.style.border = "5px solid blue";
+
+// document.body.appendChild(document.createElement('p'));
+// posts[2].appendChild(document.createElement('p'));
+
+
+const func = () => {
+    var posts = document.getElementsByTagName('article');
+    // for (var i = 0; i < posts.length; i++) {
+    //     let el = document.createElement('p');
+    //     console.log('eo2');
+    //     posts[i].appendChild(el);
+    // }
+    console.log(posts);
+}
+
+
+if (document.readyState === 'complete') {
+    func();
+} else {
+    document.onreadystatechange = function () {
+        if (document.readyState == "complete") {
+            func();
+        }
+    }
+}
